7e832ae324a7ea568f7084f0a1ddd32da378b442
Barrie Hadfield <barrie@workshare.com>
2017-09-18 05:48:53 +0200

    with reload.js

--- app/application.js.rb
+++ app/application.js.rb
@@ -1,10 +1,6 @@
 require_tree './components'
 require_tree './stores'
 
-# next two lines only needed for development (adds approx. 600k to output)
-require 'opal_hot_reloader'
-OpalHotReloader.listen(25222, false, 1)
-
 Document.ready? do
   Element['#site'].render{ HelloWorld() }
 end
