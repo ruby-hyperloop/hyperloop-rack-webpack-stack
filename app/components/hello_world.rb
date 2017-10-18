1185a716fc9c415efd281f24040d97f3835d19da
Billy.Zheng <zw963@163.com>
2017-10-19 00:35:44 +0800

    Revert "update react cdn to 15.6.2"
    This reverts commit 656235bfd52060c1ddf5ef0b16dbc83a48358a55.

--- app/components/hello_world.rb
+++ app/components/hello_world.rb
@@ -1,7 +1,7 @@
 class HelloWorld < Hyperloop::Component
   render(DIV) do
     1000.times do
-      SPAN(class: 'red-text') { "Hello World " }
+      SPAN(class: 'red-text') { 'Hello World ' }
     end
   end
 end
